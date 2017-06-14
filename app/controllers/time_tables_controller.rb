3c8880421062530ce306509f206fdbf70dc0615f
Ankush <ankushkatar796@gmail.com>
2017-06-14 13:08:19 +0530

    change navigation

--- app/controllers/time_tables_controller.rb
+++ app/controllers/time_tables_controller.rb
@@ -1,6 +1,6 @@
 class TimeTablesController < ApplicationController
   before_action :set_time_table, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, :except => [:index]
+  before_action :authenticate_user!
 
   # GET /time_tables
   # GET /time_tables.json
